129e67a7ebb793b5c9c148ae88b2199618109f39
khadijakhan <38268602+khadijakhan@users.noreply.github.com>
2018-04-20 03:46:12 +0500

    Update cloudRisks.php

--- cloudRisks.php
+++ cloudRisks.php
@@ -37,18 +37,56 @@
 	       die("Connection failed: " . $conn->connect_error);
 	   } 
 	
-	   $sql = "SELECT obsName, obsDetail FROM repository.obstical;";
+	   $sql = "SELECT obstical.obsID, obstical.obsName, obstical.obsDetail, sor.sID FROM repository.obstical LEFT JOIN sor ON obstical.obsID = sor.obsID;";
 	   $result = $conn->query($sql);
+	   $countobs=-1;  $check=0;  //to keep track of each obstacle
 
-
-   	   if ($result->num_rows > 0) 
+	   if ($result->num_rows > 0) 
 	   {
-		echo "<table><tr><th>Obstacle</th><th>Definition</th></tr>";
+		echo "<table><tr><th>#</th><th>Obstacle</th><th>Definition</th><th>Study</th></tr>";
 
+		
 	        // output data of each row
 	        while($row = $result->fetch_assoc())
 		{
-	           echo "<tr><td>" . $row["obsName"]. "</td> <td>" . $row["obsDetail"]. "</td></tr>";
+
+   	           //set check to obs-id so that we know which obstacle is currently being displayed.
+		   $check= $row["obsID"];
+
+		   //for first entry
+		   if($countobs==-1)
+		   {
+			echo "<tr><td>O" . $row["obsID"]. "</td><td>" .$row["obsName"]. "</td> <td>" . $row["obsDetail"]. "</td><td>S". $row["sID"];
+			$countobs=0;
+	  	   }
+
+		   //if obstacle id has changed from the previous iteration, new row of obstacles will be created. Otherwise we'll keep adding sources to the previous row.
+		   else if($countobs!=$check) 
+		   { 
+			//if there is no source, we will not type 'S' in the Study field
+			if($row["sID"]!=null)
+			{
+	           	   echo "</td></tr><tr><td>O" . $row["obsID"]. "</td><td>" .$row["obsName"]. "</td> <td>" . $row["obsDetail"]. "</td><td>S". $row["sID"] ;
+			   $countgoal= $row["obsID"];  //to keep count of goal
+			}
+
+			//but if there is a source, we will type 'S' in the Study field before sID.
+			else
+			{
+	           	   echo "</td></tr><tr><td>O" . $row["obsID"]. "</td><td>" .$row["obsName"]. "</td> <td>" . $row["obsDetail"]. "</td><td>". $row["sID"] ;
+			   $countgoal= $row["obsID"];
+			}
+		   }
+
+	   	   else
+		   {
+			echo ", S". $row["sID"];
+			
+		   }
+
+		   //setting it at the end of loop to see if obstacle ID changes in next iteration
+		   $countobs=$row["obsID"];   
+		
 	        }
 		
 		echo "</table>";
