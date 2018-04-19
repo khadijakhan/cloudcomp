a4419c7edc3ad12fab827e3907f740f9a4bd3be2
khadijakhan <38268602+khadijakhan@users.noreply.github.com>
2018-04-20 03:44:09 +0500

    Update cloudGoals.php

--- cloudGoals.php
+++ cloudGoals.php
@@ -37,18 +37,55 @@
 	       die("Connection failed: " . $conn->connect_error);
 	   } 
 	
-	   $sql = "SELECT gName, gDetail FROM repository.goal;";
+	   $sql = "SELECT goal.gID, goal.gName, goal.gDetail, sgr.sID FROM repository.goal LEFT JOIN sgr ON goal.gID = sgr.gID;";
 	   $result = $conn->query($sql);
-
+	   $countgoal=-1;  $check=0;  //to keep track of each goal
 
    	   if ($result->num_rows > 0) 
 	   {
-		echo "<table><tr><th>Quality Goal</th><th>Explaination (from cloud service consumer perspective) </th></tr>";
+		echo "<table><tr><th>Goal ID</th><th>Quality Goal</th><th>Explaination (from cloud service consumer perspective) </th><th>Study</th></tr>";
 
+		
 	        // output data of each row
 	        while($row = $result->fetch_assoc())
 		{
-	           echo "<tr><td>" . $row["gName"]. "</td> <td>" . $row["gDetail"]. "</td></tr>";
+		   //set check to goal-id so that we know which goal is currently being displayed.
+		   $check= $row["gID"];
+
+		   //for first entry
+		   if($countgoal==-1)
+		   {
+			echo "<tr><td>G" . $row["gID"]. "</td><td>" .$row["gName"]. "</td> <td>" . $row["gDetail"]. "</td><td>S". $row["sID"];
+			$countgoal=0;
+	  	   }
+
+		   //if goal id has changed from the previous iteration, new row of goal will be created. Otherwise we'll keep adding sources to the previous row.
+		   else if($countgoal!=$check) 
+		   { 
+			//if there is no source, we will not type 'S' in the Study field
+			if($row["sID"]!=null)
+			{
+	           	   echo "</td></tr><tr><td>G" . $row["gID"]. "</td><td>" .$row["gName"]. "</td> <td>" . $row["gDetail"]. "</td><td>S". $row["sID"] ;
+			   $countgoal= $row["gID"];  //to keep count of goal
+			}
+
+			//but if there is a source, we will type 'S' in the Study field before sID.
+			else
+			{
+	           	   echo "</td></tr><tr><td>G" . $row["gID"]. "</td><td>" .$row["gName"]. "</td> <td>" . $row["gDetail"]. "</td><td>". $row["sID"] ;
+			   $countgoal= $row["gID"];
+			}
+		   }
+
+	   	   else
+		   {
+			echo ", S". $row["sID"];
+			
+		   }
+
+		   //setting it at the end of loop to see if goal ID changes in next iteration
+		   $countgoal=$row["gID"];   
+		
 	        }
 		
 		echo "</table>";
