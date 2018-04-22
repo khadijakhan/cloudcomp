50b74374d14bcf8fc983e14fcee1a4796e84b116
khadijakhan <38268602+khadijakhan@users.noreply.github.com>
2018-04-23 02:47:13 +0500

    Update cloudTactics.php

--- cloudTactics.php
+++ cloudTactics.php
@@ -19,50 +19,138 @@
 	<a href="contactUs.php">CONTACT US </a>
    </nav>
 
-
    <div id="section">
 
-	<h3>The quality goals are: </h3>
+	<table>
 
-	<?php
-	   define ('servername' , 'localhost');
-	   define ('username' , 'clouduser');
-	   define ('password' , 'clouduser2018');
-	   define ('database' , 'repository');
+	   <?php 
+		define ('servername' , 'localhost');
+	   	define ('username' , 'clouduser');
+	   	define ('password' , 'clouduser2018');
+	   	define ('database' , 'repository');
 
-	   // Create connection
-	   $conn = new mysqli(servername, username, password, database);
-	   // Check connection
-	   if ($conn->connect_error) 
-	   {
-	       die("Connection failed: " . $conn->connect_error);
-	   } 
-	
-	   $sql = "SELECT tName, tDetail FROM repository.tactic;";
-	   $result = $conn->query($sql);
+	   	// Create connection
+	   	$conn = new mysqli(servername, username, password, database);
+	   	// Check connection
+	   	if ($conn->connect_error) 
+	   	{
+	       	   die("Connection failed: " . $conn->connect_error);
+	   	} 
+
+		$sql = "SELECT COUNT(*) AS 'C' FROM repository.tactic;";
+	   	$result = $conn->query($sql);
+
+		if ($result->num_rows > 0) 
+	   	{    // output data of each row
+	             while($row = $result->fetch_assoc())
+		     {	$max= $row["C"];   }
+		}
+	   ?>
+
+	   <tr><th>#</th><th>Resolution Tactic</th><th>Definition </th><th>Relation to obstacle</th><th>Source</th><th>Category</th></tr></tr>
+
+	   <?php for($x=1; $x <= $max; $x++) { ?>
+	     <tr>
+
+		<td> 
+
+		     <?php  echo "T". $x ; ?> 
+
+		</td>
+
+		<td>
+		     <?php
+
+			$sql = "SELECT tName FROM repository.tactic WHERE tID = ". $x .";";
+	   		$result = $conn->query($sql);
+
+			if ($result->num_rows > 0) 
+	   		{    // output data of each row
+	             	     while($row = $result->fetch_assoc())
+		             {	echo $row["tName"];   }
+			}
+ 
+		     ?>
+		</td>
+
+		<td>
+
+		     <?php
+
+			$sql = "SELECT tDetail FROM repository.tactic WHERE tID = ". $x .";";
+	   		$result = $conn->query($sql);
+
+			if ($result->num_rows > 0) 
+	   		{    // output data of each row
+	             	     while($row = $result->fetch_assoc())
+		             {	echo $row["tDetail"];   }
+			}
+
+		     ?>
+
+		</td>
+
+		<td>
+
+		     <?php
+			
+			$sql = "SELECT obsID FROM ( repository.tactic LEFT JOIN repository.tor ON tactic.tID=tor.tID ) WHERE tactic.tID = ". $x .";";
+	   		$result = $conn->query($sql);
+
+			if ($result->num_rows > 0) 
+	   		{    // output data of each row
+	             	     while($row = $result->fetch_assoc())
+		             {	echo "O". $row["obsID"] .", ";   }
+			}
+
+		     ?>
+
+		</td>
+
+		<td>
+
+		     <?php
+
+			$sql = "SELECT sID FROM ( repository.tactic LEFT JOIN repository.str ON tactic.tID=str.tID ) WHERE tactic.tID = ". $x .";";
+	   		$result = $conn->query($sql);
+
+			if ($result->num_rows > 0) 
+	   		{    // output data of each row
+	             	     while($row = $result->fetch_assoc())
+		             {	if($row["sID"] != NULL ) {  echo "S". $row["sID"] .", ";   } }
+			}
+
+		     ?>
+
+		</td>
+
+		<td>
+
+		     <?php
+
+			$sql = "SELECT cName 
+				FROM (( repository.tactic 
+				LEFT JOIN repository.tcr ON tactic.tID = tcr.tID ) 
+				LEFT JOIN repository. category ON tcr.cID = category. cID) 
+				WHERE tactic.tID = ". $x .";";
+
+	   		$result = $conn->query($sql);
+
+			if ($result->num_rows > 0) 
+	   		{    // output data of each row
+	             	     while($row = $result->fetch_assoc())
+		             {	echo $row["cName"] .", ";    }
+			}
 
 
-   	   if ($result->num_rows > 0) 
-	   {
-		echo "<table><tr><th>Resolution Tactic</th><th>Definition </th></tr>";
+		     ?>
 
-	        // output data of each row
-	        while($row = $result->fetch_assoc())
-		{
-	           echo "<tr><td>" . $row["tName"]. "</td> <td>" . $row["tDetail"]. "</td></tr>";
-	        }
-		
-		echo "</table>";
-	   }
+		</td>
 
-	   else 
-	   {
-		echo "0 results";
-	   }
+	     </tr>
+	   <?php } $conn->close(); ?>
 
-	   $conn->close();	
-	?>
-
+	</table>
 
    </div>
 
